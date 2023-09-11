2fef1b5c70084f1eeee535cd1f344d58bcf7aa9a
JamesThreadgill97 <jpgthredagill97@gmail.com>
2023-09-11 14:39:53 +0100

    Added notes and fetch function

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 const fruitForm = document.querySelector("#inputSection form");
+const fruitList = document.querySelector("#fruitSection ul");
+const fruitNutrition = document.querySelector("#nutritionSection p");
+// Event Listeners
+fruitForm.addEventListener(`submit`, extractFruit);
+// fruitList.addEventListener(`click`, removeFruit);
 
-fruitForm.addEventListener(`submit`, (e) => {
+// Functions
+function extractFruit(e) {
     e.preventDefault();
-    console.log(e)
-})
+    let fruit = e.target.fruitInput.value;
+    if (fruit) {
+        addFruit(fruit)
+        e.target.fruitInput.value = ""
+    }
+    
+}
+// Function that add the user inputted value into a li in section with the ul tag
+function addFruit(fruit) {
+    // Create a list element
+    const li = document.createElement(`li`)
+    // Give that list element some text
+    li.textContent = fruit;
+    // Add on click event for delete list items
+    li.addEventListener(`click`, removeFruit);
+    // Add that list element to the ul
+    // append vs appendChild
+    fruitList.appendChild(li);
+}
+// Function that removes the target element
+function removeFruit(e) {
+    e.target.remove();
+}
+
+// Fetching data from fruit api
+function fetchFruitData(fruit) {
+    fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
+        .then((resp) => resp.json()) /* response object (could be res, or response, as long as they are the same) */ 
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err))
+}
